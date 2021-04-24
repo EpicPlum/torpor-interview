732deaff466eb10b55821d9755cddb14b959d53d
cmj1231 <willem.kupec@gmail.com>
2021-04-25 01:36:29 +0200

    began adding JSON implementation to Codex scene

--- Torpor Interview/Assets/Scripts/Codex_Entries.cs
+++ Torpor Interview/Assets/Scripts/Codex_Entries.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEditor;
+using System.IO;
 
 public class Codex_Entries : MonoBehaviour
 {
+    private fileData data;
 
     public void setAllInactive()
     {
@@ -44,13 +46,35 @@ public class Codex_Entries : MonoBehaviour
         gameObject.tag = "EntryButton";
         gameObject.transform.GetChild(0).gameObject.tag = "Entry";
     }
+
+    public class fileData
+    {
+        public string topic1, topic2, topic3, topic4, topic5;
+    }
+
+    public void assignData()
+    {
+        
+        string json = File.ReadAllText(@"C:\Users\Admin\Documents\GitHub\torpor-interview\Torpor Interview\Assets\JSON\characters.json");
+        Debug.Log(json);
+        // data = fileData Object
+        // topic1 = 
+        data = JsonUtility.FromJson<fileData>(json);
+        Debug.Log(data.ToString());
+        Debug.Log(data.topic2.ToString());
+        Debug.Log(data.topic3.ToString());
+        Debug.Log(data.topic4.ToString());
+        Debug.Log(data.topic5.ToString());
+    }
     
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+         data = new fileData();
+
+        assignData();
     }
 
     // Update is called once per frame

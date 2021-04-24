3b079cee130fdd756bec09e97889ec49f8140336
cmj1231 <willem.kupec@gmail.com>
2021-04-24 22:49:30 +0200

    modified codex to be more visually pleasing
    fixed SpriteSwaps of a few buttons and change aspect ratio of a few images

--- Torpor Interview/Assets/Scripts/addNote.cs
+++ Torpor Interview/Assets/Scripts/addNote.cs
@@ -9,7 +9,12 @@ public class addNote : MonoBehaviour
 {
     public InputField input;
     public TextAsset output;
-    public StreamWriter writer;
+    public GameObject bullet;
+    private Transform bulletParent;
+    private string path;
+    private Vector3 lastBulletPos;
+
+
 
 
     public class inputData
@@ -19,32 +24,54 @@ public class addNote : MonoBehaviour
 
     public void addInput()
     {
-        if(GameObject.Find("Act1").activeInHierarchy)
+        if(GameObject.Find("Act1").activeInHierarchy && GameObject.Find("Act1") != null)
         {
-            writer = new StreamWriter("act1.json");
+            bulletParent = GameObject.Find("Act1").transform;
+            path = @"C:\Users\Admin\Documents\GitHub\torpor-interview\Torpor Interview\Assets\JSON\act1.json";
+            Debug.Log("Act1");
         }
-
-        if (GameObject.Find("Act2").activeInHierarchy)
+        else if (GameObject.Find("Act2").activeInHierarchy && GameObject.Find("Act2") != null)
         {
-            writer = new StreamWriter("act2.json");
+            bulletParent = GameObject.Find("Act2").transform;
+            path = @"C:\Users\Admin\Documents\GitHub\torpor-interview\Torpor Interview\Assets\JSON\act2.json";
+            Debug.Log("Act2");
         }
-
-        if (GameObject.Find("Act3").activeInHierarchy)
+        else if (GameObject.Find("Act3").activeInHierarchy && GameObject.Find("Act3") != null)
         {
-            writer = new StreamWriter("act3.json");
+            bulletParent = GameObject.Find("Act3").transform;
+            path = @"C:\Users\Admin\Documents\GitHub\torpor-interview\Torpor Interview\Assets\JSON\act3.json";
+            Debug.Log("Act3");
         }
-
-        if (GameObject.Find("Act4").activeInHierarchy)
+        else if (GameObject.Find("Act4").activeInHierarchy && GameObject.Find("Act4") != null)
         {
-            writer = new StreamWriter("act4.json");
+            bulletParent = GameObject.Find("Act4").transform;
+            path = @"C:\Users\Admin\Documents\GitHub\torpor-interview\Torpor Interview\Assets\JSON\act4.json";
+            Debug.Log("Act4");
         }
 
         inputData data = new inputData();
         data.inputText = input.text;
 
         string json = JsonUtility.ToJson(data);
+       
+        File.WriteAllText(path, json);
+        Debug.Log("Written");
 
-        writer.Write(json);
+        //retrieve json data
+        json = File.ReadAllText(path);
+        data = JsonUtility.FromJson<inputData>(json);
+        Debug.Log("Read");
+
+        lastBulletPos = new Vector3(-316, 235, 0);
+
+        //create bullet object from prefab
+        //use last y-value to determine position
+        //instantiate as child of Act Object
+        GameObject bulletObj = Instantiate(bullet, new Vector3(0,0,0), Quaternion.identity, bulletParent);
+        //bulletObj.GetComponent<Text>().text = data.inputText;
+        //bulletObj.transform.position = lastBulletPos;
+
+        lastBulletPos.y -= 30;
     }
 
     // Start is called before the first frame update

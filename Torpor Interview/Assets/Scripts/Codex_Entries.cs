56b9aa01c128c16bb4dbccba84445cb2836bad35
cmj1231 <willem.kupec@gmail.com>
2021-04-25 13:36:42 +0200

    Partial functionality for Codex JSON data retrieval

--- Torpor Interview/Assets/Scripts/Codex_Entries.cs
+++ Torpor Interview/Assets/Scripts/Codex_Entries.cs
@@ -4,23 +4,38 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEditor;
 using System.IO;
+using System;
 
 public class Codex_Entries : MonoBehaviour
 {
-    private fileData data;
+    private topicData tData;
+    private codexData cData;
+    private string codexPath = @"C:\Users\Admin\Documents\GitHub\torpor-interview\Torpor Interview\Assets\JSON\codex.json";
+    private string characterPath = @"C:\Users\Admin\Documents\GitHub\torpor-interview\Torpor Interview\Assets\JSON\characters.json";
+    private string locationsPath = @"C:\Users\Admin\Documents\GitHub\torpor-interview\Torpor Interview\Assets\JSON\locations.json";
+    private string organisationsPath = @"C:\Users\Admin\Documents\GitHub\torpor-interview\Torpor Interview\Assets\JSON\organisations.json";
+    private string historyPath = @"C:\Users\Admin\Documents\GitHub\torpor-interview\Torpor Interview\Assets\JSON\history.json";
+    private GameObject[] entries;
+    private GameObject[] entryButtons;
+    private GameObject[] titles;
+    private GameObject[] images;
+    private GameObject[] paragraphs;
+
+
+
 
     public void setAllInactive()
     {
+        entries = GameObject.FindGameObjectsWithTag("Entry");
+        entryButtons = GameObject.FindGameObjectsWithTag("EntryButton");
 
-
-        //GameObject entryButton;
-        //GameObject entry;
-        gameObject.tag = "CurrEntryButton";
+    //GameObject entryButton;
+    //GameObject entry;
+    gameObject.tag = "CurrEntryButton";
         gameObject.transform.GetChild(0).gameObject.tag = "CurrEntry";
-        var entries = GameObject.FindGameObjectsWithTag("Entry");
-        var entryButtons = GameObject.FindGameObjectsWithTag("EntryButton");
+        
 
-        foreach(var i in entries)
+        foreach(GameObject i in entries)
         {
             //entryButton = GameObject.FindWithTag("EntryButton");
             i.SetActive(false);
@@ -32,7 +47,7 @@ public class Codex_Entries : MonoBehaviour
 
         }
 
-        foreach (var i in entryButtons)
+        foreach (GameObject i in entryButtons)
         {
             //entryButton = GameObject.FindWithTag("EntryButton");
             //i.SetActive(false);
@@ -47,24 +62,65 @@ public class Codex_Entries : MonoBehaviour
         gameObject.transform.GetChild(0).gameObject.tag = "Entry";
     }
 
-    public class fileData
+    [Serializable]
+    public class topicData
     {
-        public string topic1, topic2, topic3, topic4, topic5;
+        public string[] topic1, topic2, topic3, topic4, topic5;
+    }
+
+    [Serializable]
+    public class codexData
+    {
+        public string[] category1, category2, category3, category4;
     }
 
     public void assignData()
     {
+        entryButtons = GameObject.FindGameObjectsWithTag("EntryButton");
+        titles = GameObject.FindGameObjectsWithTag("Title");
+        images = GameObject.FindGameObjectsWithTag("Image");
+        paragraphs = GameObject.FindGameObjectsWithTag("Paragraph");
+
+    string json = File.ReadAllText(characterPath);
+            Debug.Log(json);
+
+            // data = fileData Object
+            // topic1 = Cabinet
+            // topic2 = Family
+            JsonUtility.FromJsonOverwrite(json, tData);
+
+        int buttonCount = 0;
+        int titleCount = 0;
+
+
+                foreach(GameObject i in entryButtons)
+                {
+                    //set text on entry buttons
+                    i.GetComponent<Text>().text = tData.topic1[buttonCount];
+                    
+                    //set paragraphs
+
+                Debug.Log(titles[buttonCount]);
+            buttonCount++;
+                }
+
+                foreach (GameObject i in titles)
+                {
+                    //set titles
+                    i.GetComponent<Text>().text = tData.topic1[titleCount].ToUpper();
+
+            //set paragraphs
+            titleCount++;
+                }
         
-        string json = File.ReadAllText(@"C:\Users\Admin\Documents\GitHub\torpor-interview\Torpor Interview\Assets\JSON\characters.json");
-        Debug.Log(json);
-        // data = fileData Object
-        // topic1 = 
-        data = JsonUtility.FromJson<fileData>(json);
-        Debug.Log(data.ToString());
-        Debug.Log(data.topic2.ToString());
-        Debug.Log(data.topic3.ToString());
-        Debug.Log(data.topic4.ToString());
-        Debug.Log(data.topic5.ToString());
+
+
+
+
+
+
+        Debug.Log(tData.topic2[0]);
+
     }
     
 
@@ -72,7 +128,7 @@ public class Codex_Entries : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-         data = new fileData();
+         tData = new topicData();
 
         assignData();
     }
